85e83d1fd36a3b8a6b5b4ba6cb717fdb00d25581
JI-76 <johnison76@hotmail.com>
2020-10-11 10:38:11 -0700

    Animation CSS

--- navigationnation.js
+++ navigationnation.js
@@ -14,21 +14,40 @@ function toggleNav() {
     // Toggle Menu Bars Open/Closed (X)
     menuBars.classList.toggle('change');
     
-    // Toggle Menu Active
+    // Toggle Menu Active (visible or not)
     overlay.classList.toggle('overlay-active');
-
     if (overlay.classList.contains('overlay-active')) {
-        // Animate In - Overlay
+        // Animate Slide-In - Overlay
         overlay.classList.remove('overlay-slide-left');
         overlay.classList.add('overlay-slide-right');
+        // Animate Slide-In - Nav Items
+        nav1.classList.remove('slide-out-1');
+        nav1.classList.add('slide-in-1');
+        nav2.classList.remove('slide-out-2');
+        nav2.classList.add('slide-in-2');
+        nav3.classList.remove('slide-out-3');
+        nav3.classList.add('slide-in-3');
+        nav4.classList.remove('slide-out-4');
+        nav4.classList.add('slide-in-4');
+        nav5.classList.remove('slide-out-5');
+        nav5.classList.add('slide-in-5');
     } else {
-        // Animate Out - Overlay
+        // Animate Slide-Out - Overlay
         overlay.classList.remove('overlay-slide-right');
         overlay.classList.add('overlay-slide-left');
+         // Animate Slide-Out - Nav Items
+         nav1.classList.remove('slide-in-1');
+         nav1.classList.add('slide-out-1');
+         nav2.classList.remove('slide-in-2');
+         nav2.classList.add('slide-out-2');
+         nav3.classList.remove('slide-in-3');
+         nav3.classList.add('slide-out-3');
+         nav4.classList.remove('slide-in-4');
+         nav4.classList.add('slide-out-4');
+         nav5.classList.remove('slide-in-5');
+         nav5.classList.add('slide-out-5');
     };
 };
-
-
 
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
